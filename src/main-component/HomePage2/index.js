0356b455657010c5ae136b3a4d4550bc0f052344
Al Fahad <alfahadiqbal@gmail.com>
2025-09-10 16:51:10 +0500

    video updated

--- src/main-component/HomePage2/index.js
+++ src/main-component/HomePage2/index.js
@@ -30,7 +30,7 @@ const HomePage2 =() => {
                 showContactForm={false}
                 textPosition="center"
                 title="Top Positioned Text"
-                videoUrl = 'https://www.youtube.com/embed/uI24t0e6sVU?autoplay=1&mute=1&loop=1&playlist=uI24t0e6sVU&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
+                videoUrl = 'https://www.youtube.com/embed/trmn0svn7F0?autoplay=1&mute=1&loop=1&playlist=trmn0svn7F0&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
             />
             <About2/>
             <ServiceSection sClass={'section-bg'}/>
