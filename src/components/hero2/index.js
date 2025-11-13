a46d346af55c03ad2b75acff3f9bf25fc0b3afda
Al Fahad <alfahadiqbal@gmail.com>
2025-11-13 17:48:03 +0500

    Final Hero wrapper changes

--- src/components/hero2/index.js
+++ src/components/hero2/index.js
@@ -17,17 +17,9 @@ class Hero2 extends Component {
         // Default props
         const {
             heroClass = '',
-            showContactForm = true,
-            title = 'Giga Group',
-            subtitle = 'From luxury residences to world-class commercial spaces. 70 years of delivering iconic developments across Pakistan and the UAE.',
-            showTitle = true,
-            showSubtitle = true,
-            showButton = false,
-            buttonText = 'Discover More',
-            buttonLink = '/about',
             videoUrl = 'https://www.youtube.com/embed/lxOWww4vfmY?autoplay=1&mute=1&loop=1&playlist=lxOWww4vfmY&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3',
             mobileVideoUrl = 'https://www.youtube.com/embed/lxOWww4vfmY?autoplay=1&mute=1&loop=1&playlist=lxOWww4vfmY&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3',
-            textPosition = 'center' // 'center' or 'top'
+            children
         } = this.props;
 
         var settings = {
@@ -41,15 +33,6 @@ class Hero2 extends Component {
             fade: true
         };
 
-        // Determine text class based on whether contact form is shown and text position
-        const textClass = showContactForm ? 'mobile-hidden-text' : `responsive-text responsive-text-${textPosition}`;
-        
-        // Debug logging to verify props
-        console.log('Hero2 showContactForm prop:', showContactForm);
-        console.log('Hero2 textPosition prop:', textPosition);
-        console.log('Hero2 showTitle prop:', showTitle);
-        console.log('Hero2 showSubtitle prop:', showSubtitle);
-
         return (
             <section>
                 <div className={`wpo-hero-slider wpo-hero-style-2 ${heroClass}`}>
@@ -57,7 +40,7 @@ class Hero2 extends Component {
                         <div className="hero-wrapper">
                             <Slider {...settings}>
                                 <div className="hero-slide">
-                                    <div className="slide-inner video-slide" style={{height: '100vh', width: '100vw'}}>
+                                    <div className="slide-inner video-slide" style={{ height: '100vh', width: '100vw' }}>
                                         <div className="video-background">
                                             {/* Desktop Video */}
                                             <iframe
@@ -67,8 +50,8 @@ class Hero2 extends Component {
                                                 frameBorder="0"
                                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                                 allowFullScreen
-                                                ></iframe>
-                                            
+                                            ></iframe>
+
                                             {/* Mobile Video */}
                                             <iframe
                                                 className="mobile-video"
@@ -77,31 +60,12 @@ class Hero2 extends Component {
                                                 frameBorder="0"
                                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                                 allowFullScreen
-                                                ></iframe>
-                                            
+                                            ></iframe>
+
                                             <div className="video-overlay"></div>
                                         </div>
                                         <div className="container-fluid">
-                                            <div className="slide-content">
-                                                <div className={`content-wrapper ${textClass}`}>
-                                                    {showTitle && (
-                                                        <div className="slide-title">
-                                                            <h2>{title}</h2>
-                                                        </div>
-                                                    )}
-                                                    {showSubtitle && (
-                                                        <div className="slide-title-sub">
-                                                            <p>{subtitle}</p>
-                                                        </div>
-                                                    )}
-                                                </div>
-                                                <div className="clearfix"></div>
-                                                {showButton && (
-                                                    <div className="slide-btns">
-                                                        <Link to={buttonLink} className="theme-btn">{buttonText}</Link>
-                                                    </div>
-                                                )}
-                                            </div>
+                                            {children}
                                         </div>
                                     </div>
                                 </div>
@@ -142,8 +106,7 @@ class Hero2 extends Component {
                                     </div>
                                 </div> */}
                             </Slider>
-                            {/* Conditionally render Registration Form Overlay */}
-                            {showContactForm && <RegistrationForm />}
+                             {/* <RegistrationForm /> */}
                         </div>
                     </div>
                 </div>
--- src/main-component/HomePage2/index.js
+++ src/main-component/HomePage2/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 // import Navbar from '../../components/Navbar' // Old navbar commented out
 import TransparentNavbar from '../../components/TransparentNavbar' // New transparent navbar
 import Hero2 from '../../components/hero2';
@@ -11,40 +11,94 @@ import BlogSection from '../../components/BlogSection';
 import Footer from '../../components/footer';
 import Scrollbar from '../../components/scrollbar'
 import Logo from '../../images/logo.svg'
+import { Link } from 'react-router-dom';
 
 
-const HomePage2 =() => {
-    return(
+const HomePage2 = () => {
+    return (
         <Fragment>
             {/* <Navbar Logo={Logo}/> */} {/* Old navbar commented out */}
-            <TransparentNavbar Logo={Logo}/> {/* New transparent navbar */}
-            <Hero2/>
-            <Hero2 
-                showContactForm={false}
-                textPosition="top"
-                title="Gig City"
-                showTitle={false} 
-                showSubtitle={false} 
-                videoUrl = 'https://www.youtube.com/embed/uI24t0e6sVU?autoplay=1&mute=1&loop=1&playlist=uI24t0e6sVU&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
-                mobileVideoUrl = 'https://www.youtube.com/embed/uI24t0e6sVU?autoplay=1&mute=1&loop=1&playlist=uI24t0e6sVU&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
-            />
+            <TransparentNavbar Logo={Logo} /> {/* New transparent navbar */}
+            <Hero2>
+                <div className="slide-content">
+                    <div className={`c-wrapper`}>
+                        <div>
+                            <h1 className="l7-text">Giga Group</h1>
+                            <p className='l2-text'>DELIVERING EXCELLENCE SINCE 1956</p>
+                        </div>
+                        <div>
+                            <p className='l3-text'>BUILDING TRUST, SHAPING INNOVATION, AND CRAFTING A FUTURE THAT STANDS TEST OF TIME</p>
+                            <p className="l8-text">Vision, Giga Group</p>
+                        </div>
+                        {/* <div>
+                            <p className='l5-text'>FROM LUXURY RESIDENCES TO WORLD-CLASS COMMERCIAL SPACES. 70 YEARS OF DELIVERING ICONIC DEVELOPMENTS ACROSS PAKISTAN AND THE UAE</p>
+                        </div> */}
+                    </div>
+                    {/* <div className="clearfix"></div> */}
+                    {/* <div className="slide-btns">
+                        <Link to='/about' className="theme-btn">Discover More</Link>
+                    </div> */}
+                </div>
+            </Hero2>
+            <Hero2
+                videoUrl='https://www.youtube.com/embed/uI24t0e6sVU?autoplay=1&mute=1&loop=1&playlist=uI24t0e6sVU&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
+                mobileVideoUrl='https://www.youtube.com/embed/uI24t0e6sVU?autoplay=1&mute=1&loop=1&playlist=uI24t0e6sVU&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
+            >
+                <div className="slide-content">
+                    <div className={`c-wrapper`}>
+                        <div>
+                            <h1 className="l7-text"> <p className='l6-text'>GigaCity</p> Islamabad</h1>
+                            <p className='l2-text'>Pakistan's no. 1 vertical community in DHA-II Islamabad</p>
+                        </div>
+                        <div>
+                            <p className='l2-text'>Developed by Giga Group</p>
+                            {/* <p className="l4-text">Vision, Giga Group</p> */}
+                        </div>
+                        {/* <div>
+                            <p className='l2-text'>Developed by Giga Group</p>
+                        </div> */}
+                    </div>
+                    {/* <div className="clearfix"></div> */}
+                    {/* <div className="slide-btns">
+                        <Link to='/about' className="theme-btn">Discover More</Link>
+                    </div> */}
+                </div>
+            </Hero2>
             {/* uI24t0e6sVU */}
-            <Hero2 
-                showContactForm={false}
-                textPosition="center"
-                title="Goldcrest Views"
-                subtitle="Giga City, Islamabad"
-                videoUrl = 'https://www.youtube.com/embed/trmn0svn7F0?autoplay=1&mute=1&loop=1&playlist=trmn0svn7F0&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
-                mobileVideoUrl = 'https://www.youtube.com/embed/bDnmFZQ-xJU?autoplay=1&mute=1&loop=1&playlist=bDnmFZQ-xJU&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
-            />
-            <About2/>
-            <ServiceSection sClass={'section-bg'}/>
-            <ProjectSectionS2/>
+            <Hero2
+                videoUrl='https://www.youtube.com/embed/trmn0svn7F0?autoplay=1&mute=1&loop=1&playlist=trmn0svn7F0&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
+                mobileVideoUrl='https://www.youtube.com/embed/bDnmFZQ-xJU?autoplay=1&mute=1&loop=1&playlist=bDnmFZQ-xJU&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3'
+            >
+                <div className="slide-content">
+                    <div className={`c-wrapper-2`}>
+                        <div>
+                            <h1 className="l1-text">Goldcrest Views</h1>
+                            {/* <p className='l2-text'>Giga City Islamabad</p> */}
+                        </div>
+                        <div>
+                            {/* <p className='l3-text'>Developed by Giga Group</p> */}
+                            <p className="l4-text">Giga City Islamabad</p>
+                        </div>
+                        <div>
+                            {/* <p className="l4-text">Giga City Islamabad</p> */}
+
+                            {/* <p className='l5-text'>FROM LUXURY RESIDENCES TO WORLD-CLASS COMMERCIAL SPACES. 70 YEARS OF DELIVERING ICONIC DEVELOPMENTS ACROSS PAKISTAN AND THE UAE</p> */}
+                        </div>
+                    </div>
+                    {/* <div className="clearfix"></div> */}
+                    {/* <div className="slide-btns">
+                        <Link to='/about' className="theme-btn">Discover More</Link>
+                    </div> */}
+                </div>
+            </Hero2>
+            <About2 />
+            <ServiceSection sClass={'section-bg'} />
+            <ProjectSectionS2 />
             {/* <TeamSection/> */}
-            <VideoTestimonials/>
+            <VideoTestimonials />
             {/* <BlogSection/> */}
-            <Footer/>
-            <Scrollbar/>
+            <Footer />
+            <Scrollbar />
         </Fragment>
     )
 };
